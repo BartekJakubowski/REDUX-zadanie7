9f0e5bdd36870c2c8e6a7ade17ef4432d31b06a1
Bartłomiej Jakubowski <bartlomiej-jakubowski@wp.pl>
2017-11-12 14:26:11 +0100

    Added code
    Working adding new comment

--- src/CommentContainer.js
+++ src/CommentContainer.js
@@ -4,11 +4,13 @@ import {thumbsUp} from './actions.js';
 import {thumbsDown} from './actions.js';
 import {removeComment} from './actions.js';
 import {editComment} from './actions.js';
+import {addComment} from './actions.js';
 
 const mapDispatchToProps = dispatch => ({
 	thumbsUp: (id) => dispatch(thumbsUp(id)),
 	thumbsDown: (id) => dispatch(thumbsDown(id)),
-	removeComment: (id) => dispatch(removeComment(id))
+	removeComment: (id) => dispatch(removeComment(id)),
+	addComment:(id) => dispatch(addComment(id))
 });
 
 export default connect(null, mapDispatchToProps)(Comment);
--- src/comment.js
+++ src/comment.js
@@ -1,15 +1,11 @@
 import React from 'react';
-/*import {thumbsUp} from './index';
-import {thumbsDown} from './index';
-import {removeComment} from './index';
-import {editComment} from './actions';*/
 
-const Comment = ({text, votes, id, thumbsUp, thumbsDown, removeComment}) => 
+const Comment = ({text, votes, id, thumbsUp, thumbsDown, removeComment, addComment}) => 
     <li>
         {text} <span>votes: <strong>{votes}</strong> </span> 
         <button onClick={() => thumbsUp(id)}>Thumb Up</button> 
         <button onClick={() => thumbsDown(id)}>Thumb Down</button>
         <button onClick={() => removeComment(id)}>x</button>
+        <button onClick={() => addComment(id)}>Add new comment</button>
     </li>;
-
 export default Comment;
